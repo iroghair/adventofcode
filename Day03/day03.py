f6ceb46b6a305d711f5d39727f4dccd522fba776
Ivo Roghair <i.roghair@tue.nl>
2022-12-03 22:38:26 +0100

    Day03 Q2 initial

--- Day03/day03.py
+++ Day03/day03.py
@@ -1,7 +1,8 @@
 import re
 import string
 
-myfile = 'test.txt'
+# Set up
+myfile = 'input.txt'
 matches = []
 total_prio = 0
 total_badge = 0
@@ -9,10 +10,17 @@ alphabet = (string.ascii_lowercase + string.ascii_uppercase)
 
 with open(myfile, 'r') as f:
   lines = f.read().rsplit()
+  # Question 1
   for line in lines:
     mid = int(len(line)/2)
     c1,c2 = line[:mid],line[mid:]
     matches.append(re.search(str('['+c1+']'),c2).group())
     total_prio = total_prio + (alphabet.find(matches[-1]) + 1)
-
+  
+  # Question 2  
+  for l in range(0,len(lines),3):
+    badge = re.search(str('['+lines[l+2]+']'),''.join(re.findall(str('['+lines[l]+']'),lines[l+1]))).group()
+    total_badge = total_badge + (alphabet.find(badge) + 1)
+    
 print(f'Total number of priorities is: {total_prio:d}')
+print(f'Total sum of badges is: {total_badge:d}')
