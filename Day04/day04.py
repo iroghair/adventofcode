75fa86ef9522ed91573559926ed03d3d716683c7
Ivo Roghair <i.roghair@tue.nl>
2022-12-04 10:17:37 +0100

    Cleanup and comments

--- Day04/day04.py
+++ Day04/day04.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 myfile = 'input.txt'
 
-# Load data as np.array
+# Load data as np.array - f[:,0] and f[:,1] contain the min-max of set 1, f[:,2] and f[:,3] those of set 2
 f = np.array(pd.read_table(myfile,sep=",|-",engine='python',header=None,dtype=np.int64))
 
+# Check if a given set is completely within its partner set (Q1)
 isWithinBounds = np.any([
   # Second set is within first
   np.all([f[:,0] >= f[:,2] , f[:,1] <= f[:,3]],axis=0),
@@ -13,11 +14,11 @@ isWithinBounds = np.any([
   np.all([f[:,0] <= f[:,2] , f[:,1] >= f[:,3]],axis=0)]
                         ,axis=0)
 
-# Now we check for partial overlap
+# Now we check for partial overlap (Q2)
 isPartlyWithinBounds = np.any([
-  # Second set is within first
+  # Overlap (set 1 <= set 2)
   np.all([f[:,0] <= f[:,3] , f[:,1] >= f[:,2]],axis=0),
-  # First set is within second
+  # Overlap (set 1 >= set 2)
   np.all([f[:,1] <= f[:,2] , f[:,0] >= f[:,3]],axis=0)]
                         ,axis=0)
 
