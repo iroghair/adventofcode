b8657584bb1fc9b2a2e4443a6c57db2904be62fd
Ivo Roghair <i.roghair@tue.nl>
2022-12-06 10:43:43 +0100

    Small refinements (needs some work still)

--- Day05/day05-2.py
+++ Day05/day05-2.py
@@ -7,19 +7,21 @@ with open(myfile,'rt') as f:
 hasInit = False  # Whether to do the initialisation
 for line,n in zip(lines,range(len(lines))):
   moveList = [int(x) for x in re.findall(r'\d+',line)]
+  # Mooier: 
+  # _,amount,_,van,_,naar = line.strip().split(" ")
   if (hasInit and moveList):
     [Nc,Fr,To] = moveList
     Fr=Fr-1
     To=To-1
     stack[To].extend(stack[Fr][-1:-Nc-1:-1][::-1])
     del stack[Fr][-1:-Nc-1:-1]
-    print(stack)
+    # print(stack)
   
   # Find container numbers  
   # Read the container stacks when the stack numbers are recognized
   elif (line.lstrip().rstrip().startswith('1') and not hasInit):
     # Count the number of stacks
-    totalStacks = int(line.lstrip().rstrip()[-1])
+    totalStacks = len(line.lstrip().rstrip().split())
     stack = [[] for i in range(totalStacks)]
     # Fill the lists (1 per stack) from bottom to top (top of list is ground floor)
     for nStack in range(totalStacks):
@@ -28,6 +30,6 @@ for line,n in zip(lines,range(len(lines))):
       stack[nStack] = [x for x in stack[nStack] if x != '']
     filter(None, stack)
     hasInit=True
-    print(stack)
+    # print(stack)
   
 print(''.join([x[-1] for x in stack]))
