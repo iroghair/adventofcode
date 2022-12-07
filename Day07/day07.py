864b618e5d03bfcb0765a581a3c96db262dca482
Ivo Roghair <i.roghair@tue.nl>
2022-12-08 00:03:57 +0100

    Still WIP - tracking subdir sizes

--- Day07/day07.py
+++ Day07/day07.py
@@ -1,14 +1,10 @@
-from dataclasses import dataclass
-
-# @dataclass
-
-
 class Dir:
     def __init__(self, name, parent=None):
         self.name = name
         self.subdirs = list()
         self.files = list()
         self.parent = parent
+        self.total_size = 0
 
     def __str__(self):
         return self.name
@@ -40,6 +36,15 @@ class Dir:
     def get_sub_file_names(self):
         return [file.get_file_name() for file in self.files]
 
+    # Traverse the child directories and obtain the total file size
+    def get_current_size(self):
+        return sum([file.get_file_size() for file in self.files])
+    
+    def get_subdir_size(self):
+        out_list = [self.name, self.get_current_size()]
+        out_list.append([dir.get_subdir_size() for dir in self.subdirs])
+        return out_list
+
 
 class File:
     def __init__(self, name, size):
@@ -51,6 +56,28 @@ class File:
 
     def get_file_name(self):
         return self.name
+
+    def get_file_size(self):
+        return int(self.size)
+
+def build_tree(lines):
+    # Builds a directory/file tree based on terminal history
+    for line in [line.split() for line in lines]:
+        if line[0] == 'cd':
+            if line[1] == '/':
+                current_dir = head_node
+            elif line[1] == '..':
+                current_dir = current_dir.parent
+            else:
+                current_dir = current_dir.add_dir(line[1])
+        elif line[0] == 'ls':
+            for n in range(1,len(line[1:]),2): # Remove the ls command
+                if line[n] == 'dir':
+                    current_dir.add_dir(line[n+1])
+                elif line[n].isnumeric():
+                    size = int(line[n])
+                    name = line[n+1]
+                    current_dir.add_file(name,size)
 
 
 head_node = Dir('/')
@@ -74,20 +101,12 @@ else:
     with open(myfile, 'r') as file:
         lines = [line for line in file.read().split('$') if line]
 
-    # Build tree
-    for line in [line.split() for line in lines]:
-        if line[0] == 'cd':
-            if line[1] == '/':
-                current_dir = head_node
-            elif line[1] == '..':
-                current_dir = current_dir.parent
-            else:
-                current_dir = current_dir.add_dir(line[1])
-        elif line[0] == 'ls':
-            for n in range(1,len(line[1:]),2): # Remove the ls command
-                if line[n] == 'dir':
-                    current_dir.add_dir(line[n+1])
-                elif line[n].isnumeric():
-                    size = int(line[n])
-                    name = line[n+1]
-                    current_dir.add_file(name,size)
+    build_tree(lines)
+
+    sizes = head_node.get_subdir_size()
+
+    # Test case: should be 
+    assert 48381165==head_node.get_subdir_size()
+    print(f'The size of root is: {head_node.get_subdir_size()}, and should be 48381165.')
+
+    # Traverse all
