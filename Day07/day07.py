a88e73fd66cae50f2c5d23578f7a3c23f899712f
Ivo Roghair <i.roghair@tue.nl>
2022-12-07 23:12:49 +0100

    Building a tree works!

--- Day07/day07.py
+++ Day07/day07.py
@@ -13,14 +13,16 @@ class Dir:
     def __str__(self):
         return self.name
 
-    # Add a new subdirectory with a unique name
+    # Add a new subdirectory with a unique name, return a reference
     def add_dir(self, name):
         if (name not in self.get_sub_dir_names()):
-            new_dir = Dir(name,self)
+            new_dir = Dir(name, self)
             self.subdirs.append(new_dir)
+            return self.subdirs[-1]
+        return self.get_dir(name)
 
     # Return a reference to a subdirectory with name
-    def get_dir(self,name):
+    def get_dir(self, name):
             a = [dir for dir in self.subdirs if dir.get_dir_name() == name]
             return a[0]
 
@@ -37,6 +39,7 @@ class Dir:
 
     def get_sub_file_names(self):
         return [file.get_file_name() for file in self.files]
+
 
 class File:
     def __init__(self, name, size):
@@ -68,17 +71,23 @@ if (False):
 else:
     myfile = 'test.txt'
 
-    with open(myfile,'r') as file:
+    with open(myfile, 'r') as file:
         lines = [line for line in file.read().split('$') if line]
 
+    # Build tree
     for line in [line.split() for line in lines]:
         if line[0] == 'cd':
             if line[1] == '/':
                 current_dir = head_node
             elif line[1] == '..':
-                current_dir = current_dir.parent()
+                current_dir = current_dir.parent
             else:
                 current_dir = current_dir.add_dir(line[1])
-        # elif line[0] == 'ls':
-        #     for result in []
-
+        elif line[0] == 'ls':
+            for n in range(1,len(line[1:]),2): # Remove the ls command
+                if line[n] == 'dir':
+                    current_dir.add_dir(line[n+1])
+                elif line[n].isnumeric():
+                    size = int(line[n])
+                    name = line[n+1]
+                    current_dir.add_file(name,size)
