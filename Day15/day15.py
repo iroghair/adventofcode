dbaec4ab94cbff5dcf073cc37b3bf9a2bb4a8f9a
Ivo Roghair <i.roghair@tue.nl>
2022-12-18 22:25:06 +0100

    Day15 Q1

--- Day15/day15.py
+++ Day15/day15.py
@@ -36,40 +36,22 @@ xmin = np.min(np.array([num[:,0]-num[:,2]]))
 ymin = np.min(np.array([num[:,1]-num[:,3]]))
 print('Max size of domain: ',xmax-xmin,ymax-ymin)
 
-# A = np.zeros( (xmax-xmin,ymax-xmin), dtype=np.int64)
 track = np.zeros((xmax-xmin,))
 offset = np.array([xmin,ymin])
-line = 10
-for n,i in enumerate{num}:
+line = 2000000
+for n in num:
     # Central coordinate
     c = (n[0],n[1])
     # Get MH distance of nearest beacon
     dist = abs(n[2]-n[0]) + abs(n[3]-n[1])
     # Get indices from central node
-    print(f'Processing line {i} with coordinates {n}\n')
+    print(f'Processing line with coordinates {n}')
     track = get_within_MH_dist_coords(dist, c, line, track, offset)
 
 for n in num:
     # Coordinate of beacon
     beacon = (n[2],n[3])
     if beacon[1] == line:
-        track[n[2]-offset[0]] = 0
+        track[n[2]-offset[0]] = 2
 
-print(f'Number of locations on line {line} where beacon cannot exist: {np.shape(np.where(track>0))[1]}')
-
-# for n in num:
-#     # Central coordinate (correct for negative offset)
-#     c = (n[0]+xmin, n[1]+ymin)
-#     # Get MH distance of nearest beacon
-#     dist = abs(n[2]-n[0]) + abs(n[3]-n[1])
-#     # Get indices from central node
-#     base = get_MH_dist_coords(dist)
-#     for ix in c[0]+base[:,0]:
-#         for iy in c[1] + base[:,1]:
-#             print(ix,iy)
-#             A[ix,iy] = -1
-
-#     print(A)
-
-    # A[, c[1]+base[1,:]] = 1
-    # A[]
+print(f'Number of locations on line {line} where beacon cannot exist: {np.shape(np.where(track==1))[1]}')
