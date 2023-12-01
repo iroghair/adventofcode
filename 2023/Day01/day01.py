986c91d621813b8cab4143d60a70d31f9b24b64c
Ivo Roghair <i.roghair@tue.nl>
2023-12-01 23:53:02 +0100

    Cleanup

--- 2023/Day01/day01.py
+++ 2023/Day01/day01.py
@@ -6,16 +6,17 @@ part2 = True
 with open(infile, 'r') as file:
     data = file.read().splitlines()
 
-sum_of_values = 0
+
+# Create replacement dictionary
 ddict = {}
 for digit,word in enumerate(['zero','one','two','three','four','five','six','seven','eight','nine']):
     ddict[word]=digit
 
-print(ddict)
+sum_of_values = 0
 
 for line in data:
-    # print(line)
     if part2:
+        # Find occurrences of word-digits
         replace_index = []
         for word,digit in ddict.items():
             for m in re.finditer(word,line):
@@ -23,6 +24,7 @@ for line in data:
                 replace_index.append((idx,digit,word))
         replace_index.sort()
 
+        # Replace last and first words with actual digits
         if replace_index:
             i = replace_index.pop(-1)
             line = line[:i[0]] + str(i[1]) + line[i[0]:]
@@ -30,10 +32,9 @@ for line in data:
             i = replace_index.pop(0)
             line = line[:i[0]] + str(i[1]) + line[i[0]:]
 
-    # print(line)
+    # Match actual digits and select first and last to create a number
     digits = re.findall('\d',line)
     calibration_value = int(digits[0]+digits[-1])
-    print(calibration_value)
     sum_of_values += calibration_value
 
 print(f'Sum of values: {sum_of_values}')
