dbb532e2dcd44b68ce02c8bb06ac18da60b88819
Ivo Roghair <i.roghair@tue.nl>
2023-12-02 16:27:14 +0100

    Add day2-2 2023

--- 2023/Day02/day02.py
+++ 2023/Day02/day02.py
@@ -1,7 +1,9 @@
 import re
 
+part1 = False
 infile = 'input.txt'
 RGBmatch = (12,13,14)
+set_power = []
 
 with open(infile,'r') as file:
     data = file.read().splitlines()
@@ -12,6 +14,7 @@ for line in data:
     game = int(header_sets[0].split()[1])
     print('Game', game)
     sum_IDs += game
+    RGBmax = [0,0,0]
     for set in header_sets[1].split(';'):
         # print(set)
         reds = re.findall(r'\d+\sred',set)
@@ -21,16 +24,18 @@ for line in data:
                     int(greens[0].split()[0]) if (greens) else 0, \
                     int(blues[0].split()[0]) if (blues) else 0)
 
-        if any(i>j for i,j in zip(RGBset,RGBmatch)):
-            print('Set: ', set, ' (RGB: ', RGBset, ' does not fit ')
-            sum_IDs -= game
-            break
-        
-print(sum_IDs)
-        
-        # greens = re.search(r'\d+\sgreen',set)[0].split()[0]
-        # blues = re.search(r'\d+\sblue',set)[0].split()[0]
-        # rgbset=(reds,0,0)
-        # print(rgbset)
-    # b = re.split(r'\b.*;',line)
-    # print(b)
+        if (part1):
+            if any(i>j for i,j in zip(RGBset,RGBmatch)):
+                print('Set: ', set, ' (RGB: ', RGBset, ' does not fit ')
+                sum_IDs -= game
+                break
+        else:
+            RGBmax = [max(i,j) for i,j in zip(RGBmax,RGBset)]
+            print(f'RGBmax: {RGBmax}, RGBset: {RGBset}')
+    set_power.append(RGBmax[0]*RGBmax[1]*RGBmax[2])
+    print(f'Game: {game} has power {set_power}')
+    
+if (part1):    
+    print(sum_IDs)
+else:
+    print(f'The total of the set powers is {sum(set_power)}')        
