d9a66b0553a383ae6d0f91ad2bdfa74a7f218936
Ivo Roghair <i.roghair@tue.nl>
2023-12-04 12:47:01 +0100

    Day4 part 1+2 2023

--- 2023/Day04/day04.py
+++ 2023/Day04/day04.py
@@ -1,40 +1,54 @@
 import re
 import numpy as np
+import time
+from functools import cache
 
 def import_data(infile):
+    """Load the infile, return a list of lines"""
     with open(infile,'r') as file:
         data = file.read().splitlines()
     return data
 
-def run_part_1(data):
+def run_part_1_2(data):
+    """For all lines, process the card and accumulate the score. Also get copies of cards."""
     ans_pt1 = 0
-    for line in data:
-        gamesep = line.split(':')
-        cardno = gamesep[0].split()[1]
-        card_numbers = gamesep[1].split('|')[0].split()
-        match_numbers = gamesep[1].split('|')[1].split()
-        winning_numbers = [x for x in card_numbers if x in match_numbers]
+    ncopies = np.zeros(len(data),dtype=np.int32)
+
+    for n,_ in enumerate(data):
+        winning_numbers = process_card(data,n)
         l = len(winning_numbers)
-        ans_pt1 += (score := 2**(l-1) if l>0 else 0)
-        # print(winning_numbers, score)     
-    return ans_pt1
+        ans_pt1 += 2**(l-1) if l>0 else 0
+        ncopies += get_copies(n)
 
-    print(f'Part 1: {ans_pt1}')
+    ans_pt2 = np.sum(ncopies)
+    return ans_pt1, ans_pt2
 
-def run_part_2(data):
-    ans_pt2 = ...
+# @cache
+def get_copies(n):
+    """Get copies of the next few cards, based on the amount of winning numbers. Recursively determine
+    if these cards win additional copies. Accumulate all copies in an array and return the array."""
+    ncopies = np.zeros(len(data),dtype=np.int32)
+    ncopies[n] += 1
+    copies = range(n+1,n+1+len(process_card(data,n)))
+    for card in copies:
+        ncopies += get_copies(card)
 
-    print(f'Part 2: {ans_pt2}')
+    return ncopies
+
+def process_card(data,cardno):
+    """Get the card and match numbers and return the winning numbers"""
+    gamesep = data[cardno].split(':')
+    card_numbers = gamesep[1].split('|')[0].split()
+    match_numbers = gamesep[1].split('|')[1].split()
+    winning_numbers = [x for x in card_numbers if x in match_numbers]
+    return winning_numbers
 
 if __name__ == '__main__':
-    part1 = True
     infile = 'input.txt'
     data = import_data(infile)
-
-    if part1:
-        print(run_part_1(data))
-    else:
-        run_part_2(data)
+    start = time.time()
+    print(run_part_1_2(data))
+    print(f'Finished in {time.time() - start} s')
         
 
    
