5d3a581dcaff7c5a520a157ad24790c920e64ba5
Ivo Roghair <i.roghair@tue.nl>
2023-12-07 00:00:45 +0100

    Cleanup day 5 and integrate all in day05.py

--- 2023/Day05/day05.py
+++ 2023/Day05/day05.py
@@ -1,8 +1,9 @@
-import re
 import numpy as np
 from itertools import islice
 import sys
+import time
 
+# If Python.__version__ < 3.12
 def batched(iterable, n):
     # batched('ABCDEFG', 3) --> ABC DEF G
     if n < 1:
@@ -26,11 +27,17 @@ class mapper:
     def map(self,src):
         for maps in self.rangelist:
             if src in maps[0]:
-                print(f'{self.name}-> src: {src} is in range: {maps[0]} and becomes {src+maps[2]}')
+                # print(f'{self.name}-> src: {src} is in range: {maps[0]} and becomes {src+maps[2]}')
                 return src+maps[2]
-        print(f'{self.name}-> src: {src} maps to {src}')
+        # print(f'{self.name}-> src: {src} maps to {src}')
         return src # Default
         
+    def map_reversed(self,src):
+        for maps in self.rangelist:
+            if src in maps[1]:
+                return src-maps[2]
+        return src # Default
+
 def import_data(infile):
     with open(infile,'r') as file:
         data = file.read()
@@ -44,6 +51,9 @@ def import_data(infile):
         data_in_set = mapdata.split()
         mlist.append(mapper(data_in_set[0],data_in_set[2:]))
 
+    return seeds,mlist
+ 
+def run_part_1(seeds,mlist):
     minloc = sys.maxsize
     for seed in seeds:
         x = seed
@@ -52,26 +62,35 @@ def import_data(infile):
         minloc = min(x,minloc)
 
     print(f'Part 1: {minloc}')
- 
-def run_part_1(data):
-    ans_pt1 = ...
 
-    print(f'Part 1: {ans_pt1}')
+def run_part_2(seeds,mlist):
+    seeds = [range(int(x[0]),int(x[0])+int(x[1])) for x in batched(seeds,2)]
+    location = -1
+    found = False
+    while not found:
+        x = (location := location + 1)
+        if location%1000000==0:
+            print(f'Testing location: {location}++')
+        for maps in mlist[::-1]:
+            x = maps.map_reversed(x)
 
-def run_part_2(data):
-    ans_pt2 = ...
+        for seedsr in seeds:
+            if (x in seedsr):
+                found = True
+                print(f'Part 2: {location}')
 
-    print(f'Part 2: {ans_pt2}')
 
 if __name__ == '__main__':
-    part1 = True
-    infile = 'test.txt'
-    data = import_data(infile)
+    infile = 'input.txt'
+    seeds,mlist = import_data(infile)
 
-    if part1:
-        run_part_1(data)
-    else:
-        run_part_2(data)
+    start_pt1 = time.time()
+    run_part_1(seeds,mlist)
+    print(f'Runtime: {(time.time() - start_pt1)*1.0e6} us')
+    
+    start_pt2 = time.time()
+    run_part_2(seeds,mlist)
+    print(f'Runtime: {time.time() - start_pt2} s')
         
 
    
