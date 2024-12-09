e636669c20b1a8fe5aedcb8f568c1e5e538f61e9
Ivo Roghair <i.roghair@tue.nl>
2024-12-09 16:43:54 +0100

    Add initial implementation for Day 09 with input parsing

---
+++ 2024/Day09/day09.py
@@ -0,0 +1,16 @@
+# day09.py
+
+def parse(infile):
+    with open(infile, 'r') as f:
+
+def main():
+    infile = 'test.txt'
+    # infile = 'test2.txt'
+    infile = 'input.txt'
+    part_1 = part_2 = 0
+
+    print(f'Part 1: {part_1}')
+    print(f'Part 2: {part_2}') # 817, 910 too low; 1319 too high
+
+if __name__ == "__main__":
+    main()
