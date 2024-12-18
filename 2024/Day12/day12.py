27e9fcf67857712fab565d21d2b60c2e34cd2406
Ivo Roghair <i.roghair@tue.nl>
2024-12-19 00:20:12 +0100

    Bit more cleanup

--- 2024/Day12/day12.py
+++ 2024/Day12/day12.py
@@ -4,7 +4,7 @@ import scipy as sp
 import matplotlib.pyplot as plt
 from strgrid import strgrid, loc # Imported from Day08, I should make it a package some day
 
-DEBUG = True
+DEBUG = False
 steps = ( loc((1,0)) , loc((-1,0)) , loc((0,1)) , loc((0,-1)) )
 
 def parse(infile):
@@ -19,7 +19,6 @@ def create_plot(garden: strgrid,start_xy):
     perimeter = 0
     outer_corners = 0
     for xy in current_plot:
-        steps_to_fence = [] # Record the steps that lead to fence
         for step in steps:
             new_xy = loc(xy)+step
             # Fill operation: add new coordinates to list if they are inside the domain and the same plant
@@ -29,64 +28,19 @@ def create_plot(garden: strgrid,start_xy):
             # Perimeter operation: if the projected block xy is outside of the domain or has another plant, we need a fence:
             if not garden.is_inside(new_xy) or garden[new_xy] != current_plant:
                 perimeter += 1
-                steps_to_fence.append(step)
-        # The total number of corners in a plot are equal to the number of sides of the plot
-        # We can find the convex corners by recording the steps from each block xy leading to a fence
-        # If we find a fence with 4 steps, the plot is a single block xy and has 4 corner points
-        # If we find a fence with 3 steps, we have 2 corner points
-        # If we find a fence with 2 steps, there is 1 corner, unless the fences are parallel
-        # Parallel fences we can compute through summing the steps; if this yields (0,0) (e.g. (+1,0) and (-1,0)) the fences are parallel on that block
-        match len(steps_to_fence):
-            case 0 | 1: # No corners on this block
-                continue                
-            case 2: # Parallel fences when the sum of both steps equals (0,0)
-                if steps_to_fence[0] + steps_to_fence[1] == (0,0):
-                    continue
-                else:
-                    outer_corners += 1
-            case 3:
-                    outer_corners += 2
-            case 4:
-                    outer_corners += 4
-    # We only detect convex corners on the plot, so the total corners of the plot is 2n-4
-    outer_corners = 2*outer_corners - 4
     
     current_plot = set(current_plot)
     
-    plot = {'Plant': current_plant, 'Coords': current_plot, 'Area': len(current_plot), 'Perimeter': perimeter, 'Outer corners': outer_corners, 'Inner corners': 0}
+    plot = {'Plant': current_plant, 'Coords': current_plot, 'Area': len(current_plot), 'Perimeter': perimeter}
     return plot
-    
-def is_island(plot,plots, garden) -> bool:
-    # plots.remove(plot)
-    # Get coords around plot
-    coords = set([loc(xy) + step for xy in plot['Coords'] for step in steps]).difference(plot['Coords'])# if garden.is_inside(loc(xy)+step)])
-    print(f'Coords around plot: {coords}')
-    
-    # Check if the plot is adjacent to the field edge, in that case not an island
-    for xy in coords:
-        if not garden.is_inside(xy):
-            return False
-    
-    neighbor_plot = garden[xy]
-    for xy in coords:
-        if garden[xy] != neighbor_plot:
-            return False
-    
-    # Once we get here, the plot is an island and xy is a coordinate in the neighboring, surrounding plot
-    # In that case, assign the number of outer corners of the current plot as inner corners
-    print(f'Plot {plot} is an island with neighbor: {neighbor_plot}')
-    for nb in plots:
-        if xy in nb['Coords']:
-            nb['Inner corners'] += plot['Outer corners']
-            print(f'Sides of plot {nb['Plant']}: {nb['Inner corners'] + nb['Outer corners']}')
-    return True
     
 def main():
     part_1 = part_2 = 0
     infile = 'input.txt'
     # infile = 'test5.txt'
     garden = parse(infile)
-    print(garden)
+    if DEBUG:
+        print(garden)
    
     plots = []
     for loc in garden.get_locations():
@@ -94,14 +48,11 @@ def main():
             plots.append(create_plot(garden,loc))
 
     part_1 = sum([plot['Area']*plot['Perimeter'] for plot in plots])
-    
-    for plot in plots:
-        is_island(plot, plots.copy(), garden)
         
+    # Part 2
     for plot in plots:
         # Get coordinates of plot
         c = np.array(list(plot['Coords']))
-        # dim =  mx-mn+1
         
         # Create a matrix containing the plot as 1 and add 0's elsewhere 
         mx,mn = np.max(c,axis=0), np.min(c,axis=0)
@@ -120,9 +71,7 @@ def main():
         part_2 += cost_of_plot
     
     print(f'Part 1: {part_1}')
-    print(f'Part 2: {part_2}')  # 869438 too low
-                                # 873584
-                                # 886726 too high
+    print(f'Part 2: {int(part_2)}')
 
 if __name__ == "__main__":
     main()
