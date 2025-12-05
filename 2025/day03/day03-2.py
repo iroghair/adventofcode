8cc58858905a4cb84131455d50d11c645a677ebe
Ivo Roghair <i.roghair@tue.nl>
2025-12-05 08:21:52 +0100

    cleanup day03

--- 2025/day03/day03-2.py
+++ 2025/day03/day03-2.py
@@ -1,9 +1,10 @@
 import numpy as np 
 
-def largest_digit(line: str):
+def largest_digit(line: str) -> tuple:
     """
-    Seeks the largest single digit in a string within a given range. 
-    Returns a tuple as (digit, index) of the digit identified. 
+    Seeks the largest, first-occurring single digit in a string 
+    Returns a tuple as (digit, index) of the digit identified.
+    Note: no handling of non-digit characters. 
     """
     numbers = list(map(int,line))
     res = (-1,0)
@@ -15,7 +16,7 @@ def largest_digit(line: str):
     return res
 
 
-def main(fname: str, length: int = 12):
+def main(fname: str, length: int = 12,debug: bool = False) -> int:
     with open(fname,'r') as f:
         data = f.read().split('\n')
 
@@ -23,23 +24,28 @@ def main(fname: str, length: int = 12):
     for line in data:
         istart = 0
         digits = []
+        # Exclude the last n characters from the string, 
+        # or we might find a max too far towards the end to reach to the right length
         for n in range(length-1,0,-1):
             digit,iskip = largest_digit(line[istart:-n])
             digits.append(digit)
             istart += iskip+1 
+        # Including the last character cannot be done with a slicing endpoint afaik, so a separate expression
         digit,iskip = largest_digit(line[istart:])
         digits.append(digit)
-            
+
         dsum = 0
         for d in digits:
             dsum = dsum*10 + d
         dsums.append(dsum)
-        print(dsum)
+        if debug:
+            print(dsum)
     
-    print(sum(dsums))
+    return sum(dsums)
 
 if __name__ == "__main__":
     fname = "test.txt"
     fname = "input.txt"
-    main(fname,12)
+    print(f'Part 1: {main(fname,2)}')
+    print(f'Part 2: {main(fname,12)}')
 
